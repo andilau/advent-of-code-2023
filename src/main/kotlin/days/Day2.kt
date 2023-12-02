8c15e8236b11efbbf4790200f05c20ea2b7ed25c
Andi Lau <andi@herrlau.de>
2023-12-02 22:28:35 +0100

    Day 2: Refactor

--- src/main/kotlin/days/Day2.kt
+++ src/main/kotlin/days/Day2.kt
@@ -5,45 +5,40 @@ package days
     url = "https://adventofcode.com/2023/day/2",
     date = Date(day = 2, year = 2023)
 )
-class Day2(private val input: List<String>) : Puzzle {
-    override fun partOne(): Int = input.map { Game.from(it) }
-        .filter { it.possible() }
-        .sumOf { it.id }
+class Day2(input: List<String>) : Puzzle {
 
-    override fun partTwo(): Int = input.map { Game.from(it) }
-        .sumOf { it.multiple() }
+    val games = input.map { Game.from(it) }
 
-    data class Game(val id: Int, val sets: List<Map<String, Int>>) {
+    override fun partOne(): Int = games.filter { it.possible() }.sumOf { it.id }
 
-        fun possible(): Boolean {
-            val reduce: Map<String, Int> = sets.reduce { m1, m2 ->
-                (m1.asSequence() + m2.asSequence()).groupBy({ it.key }, { it.value }).mapValues { it.value.max() }
-            }
+    override fun partTwo(): Int = games.sumOf { it.multiple() }
 
-            return reduce.getOrDefault("red", 0) <= 12 &&
-                    reduce.getOrDefault("green", 0) <= 13 &&
-                    reduce.getOrDefault("blue", 0) <= 14
+    data class Game(val id: Int, val sets: Map<String, Int>) {
+
+        fun possible(): Boolean = sets.run {
+            getOrDefault("red", 0) <= 12 &&
+                    getOrDefault("green", 0) <= 13 &&
+                    getOrDefault("blue", 0) <= 14
         }
 
-        fun multiple(): Int {
-            val reduce: Map<String, Int> = sets.reduce { m1, m2 ->
-                (m1.asSequence() + m2.asSequence()).groupBy({ it.key }, { it.value }).mapValues { it.value.max() }
-            }
-            println("reduce = ${reduce}")
-
-            return reduce.getOrDefault("red", 1) *
-                    reduce.getOrDefault("green", 1 ) *
-                    reduce.getOrDefault("blue", 1)
+        fun multiple(): Int = sets.run {
+            getOrDefault("red", 1) *
+                    getOrDefault("green", 1) *
+                    getOrDefault("blue", 1)
         }
-
 
         companion object {
             fun from(record: String) = Game(
                 record.substringBefore(':').substringAfter("Game ").toInt(),
                 record.substringAfter(": ").split("; ")
                     .map { it.split(", ").associate { it.split(" ").let { it[1] to it[0].toInt() } } }
+                    .reduce { m1, m2 ->
+                        (m1.asSequence() + m2.asSequence()).groupBy({ it.key }, { it.value })
+                            .mapValues { it.value.max() }
+                    }
             )
         }
+
     }
 
 }
