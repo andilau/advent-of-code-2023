0d0673973b204c8d80b294eb233cc354d8adfc01
Andi Lau <andi@herrlau.de>
2023-12-02 20:51:24 +0100

    Day 1: Trebuchet?! - Refactor

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -6,59 +6,46 @@ package days
     date = Date(day = 1, year = 2023)
 )
 class Day1(private val input: List<String>) : Puzzle {
-    override fun partOne(): Int {
-        return input
-            .map { line -> Pair(line.first { it.isDigit() }, line.last { it.isDigit() }) }
-            .map { p -> "" + p.first + p.second }
-            .sumOf { va -> va.toInt() }
-    }
+    override fun partOne(): Int = input
+        .map { line -> Pair(line.firstDigit, line.lastDigit) }
+        .sumOf { it.first * 10 + it.second }
 
-    override fun partTwo(): Int {
-        return input
-            .asSequence()
-            .map { line -> Pair(line.leftValidation(), line.rightVliadtion()) }
-            .map { p -> "" + p.first + p.second }
-            .map { va -> va.toInt() }.sum()
+    override fun partTwo(): Int = input
+        .map { line -> Pair(line.firstDigitOrNumber, line.lastDigitOrNumber) }
+        .sumOf { it.first * 10 + it.second }
 
-    }
+    private val String.firstDigit get() = first { it.isDigit() }.digitToInt()
 
-    private val letterToDigit = mapOf(
-        "one" to 1,
-        "two" to 2,
-        "three" to 3,
-        "four" to 4,
-        "five" to 5,
-        "six" to 6,
-        "seven" to 7,
-        "eight" to 8,
-        "nine" to 9
-    )
+    private val String.lastDigit get() = last { it.isDigit() }.digitToInt()
 
-    private fun String.leftValidation(): Int {
-        return windowedSequence(this.length, 1, true)
+    private val String.firstDigitOrNumber
+        get() = windowedSequence(this.length, 1, true)
             .firstNotNullOfOrNull { substring ->
                 when {
                     substring.first().isDigit() -> substring.first().digitToInt()
                     else -> letterToDigit.keys.firstOrNull() { substring.startsWith(it) }?.let { letterToDigit[it] }
                 }
-            }
-            .also { print("L:$it ") }
-            ?: error("Hää?")
-    }
+            } ?: error("Contains no number: $this")
 
-    private fun String.rightVliadtion(): Int {
-        return indices.map { this.substring(0..<length - it) }
+    private val String.lastDigitOrNumber
+        get() = indices.asSequence()
+            .map { this.substring(0..<length - it) }
             .firstNotNullOfOrNull { substring ->
                 when {
                     substring.last().isDigit() -> substring.last().digitToInt()
                     else -> letterToDigit.keys.firstOrNull() { substring.endsWith(it) }?.let { letterToDigit[it] }
                 }
-            }
-            .also { println("R:$it") }
-            ?: error("Hää?")
-    }
+            } ?: error("Contains no number: $this")
 
+    private val letterToDigit = listOf(
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine"
+    ).mapIndexed { index, s -> s to index + 1 }.toMap()
 }
-
-
-
