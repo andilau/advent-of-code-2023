3090d4dcf280e60c9e8ab1dc028d821581739c46
Andi Lau <andi@herrlau.de>
2023-12-02 19:50:37 +0100

    Day 1: Trebuchet?! - Part 2

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -6,12 +6,59 @@ package days
     date = Date(day = 1, year = 2023)
 )
 class Day1(private val input: List<String>) : Puzzle {
-    override fun partOne():Int {
-        return input.map { line-> Pair(line.first { it.isDigit() }, line.last { it.isDigit() }) }
+    override fun partOne(): Int {
+        return input
+            .map { line -> Pair(line.first { it.isDigit() }, line.last { it.isDigit() }) }
+            .map { p -> "" + p.first + p.second }
+            .sumOf { va -> va.toInt() }
+    }
+
+    override fun partTwo(): Int {
+        return input
+            .asSequence()
+            .map { line -> Pair(line.leftValidation(), line.rightVliadtion()) }
             .map { p -> "" + p.first + p.second }
             .map { va -> va.toInt() }.sum()
 
     }
 
-    override fun partTwo() = 0
+    private val letterToDigit = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+    )
+
+    private fun String.leftValidation(): Int {
+        return windowedSequence(this.length, 1, true)
+            .firstNotNullOfOrNull { substring ->
+                when {
+                    substring.first().isDigit() -> substring.first().digitToInt()
+                    else -> letterToDigit.keys.firstOrNull() { substring.startsWith(it) }?.let { letterToDigit[it] }
+                }
+            }
+            .also { print("L:$it ") }
+            ?: error("Hää?")
+    }
+
+    private fun String.rightVliadtion(): Int {
+        return indices.map { this.substring(0..<length - it) }
+            .firstNotNullOfOrNull { substring ->
+                when {
+                    substring.last().isDigit() -> substring.last().digitToInt()
+                    else -> letterToDigit.keys.firstOrNull() { substring.endsWith(it) }?.let { letterToDigit[it] }
+                }
+            }
+            .also { println("R:$it") }
+            ?: error("Hää?")
+    }
+
 }
+
+
+
--- src/test/kotlin/days/Day1Test.kt
+++ src/test/kotlin/days/Day1Test.kt
@@ -7,16 +7,16 @@ import org.junit.jupiter.api.Test
 
 @DisplayName("Day 1")
 class Day1Test {
-    val calibrationValues = """
-        1abc2
-        pqr3stu8vwx
-        a1b2c3d4e5f
-        treb7uchet
-    """.trimIndent().lines()
-
     @Nested
     @DisplayName("Part 1")
     inner class Part1 {
+        val calibrationValues = """
+            1abc2
+            pqr3stu8vwx
+            a1b2c3d4e5f
+            treb7uchet
+        """.trimIndent().lines()
+
         @Test
         fun shouldReturnSumOfCalibrationValues() {
             assertThat(Day1(calibrationValues).partOne()).isEqualTo(142)
@@ -27,9 +27,18 @@ class Day1Test {
     @Nested
     @DisplayName("Part 2")
     inner class Part2 {
+        val calibrationValues = """
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+        """.trimIndent().lines()
         @Test
-        fun shouldReturnProductOfEmptyList() {
-            assertThat(Day1(emptyList()).partTwo()).isEqualTo(1)
+        fun shouldReturnSumOfCalibrationValues() {
+            assertThat(Day1(calibrationValues).partTwo()).isEqualTo(281)
         }
 
 
